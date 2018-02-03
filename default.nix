eaf7f6c1040ef312581a06bf7a14a4ee35f4b459
Falco Peijnenburg <falco@lumiguide.nl>
2018-02-03 13:09:57 +0100

    Pinned nixpkgs

--- default.nix
+++ default.nix
@@ -1,5 +1,23 @@
-{ pkgs ? import <nixpkgs> {} }:
 let
+  # nixpkgs pinning method from
+  # Sneaking Nix at $work – and become a hero, hopefully by zimbatm
+  # See http://nixcon2017.org/#program
+  fetchTarball-compat = { url, sha256 }@attrs:
+    let
+     inherit (builtins) lessThan nixVersion fetchTarball;
+    in
+    if lessThan nixVersion "1.12" then
+     fetchTarball { inherit url; }
+    else
+     fetchTarball attrs;
+
+  nixpkgs-src = fetchTarball-compat {
+   url = "https://github.com/NixOS/nixpkgs-channels/archive/c1d9aff56e0ae52ee4705440fe09291a51e91977.tar.gz";
+   sha256 = "1dsszs9ds17n006yv4il39640msslmhdh3ckg4l0vfq8y11ljvss";
+  };
+
+  pkgs = import (nixpkgs-src) {};
+
   inherit (pkgs) runCommand closurecompiler;
   inherit (pkgs.haskell.packages) ghcjsHEAD ghc802;
 
